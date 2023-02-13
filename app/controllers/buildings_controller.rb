17e0ffaa3ac360341b543bd0185742a830f7a199
Andrew Arrow <oneone@gmail.com>
2023-02-13 12:19:38 -0800

    add_building

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -8,4 +8,24 @@ class BuildingsController < ApplicationController
     @building = Building.find_by_guid(params[:id])
   end
 
+  def new
+    @building = Building.new
+  end
+
+  def create
+    @item = Building.new(item_params)
+
+    if @item.save
+      redirect_to building_path(@item.guid), notice: "was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:building).permit(:address)
+  end
+
 end
