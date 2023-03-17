a37fd81ac36f433d7125da04abf42773f3864de1
Andrew Arrow <oneone@gmail.com>
2023-03-17 14:21:46 -0700

    clean up and fresh controller

--- app/fresh_controller.go
+++ app/fresh_controller.go
@@ -5,12 +5,14 @@ import "github.com/andrewarrow/feedback/router"
 func HandleFresh(c *router.Context, second, third string) {
 	if second == "" {
 		handleFreshIndex(c)
-	} else if third != "" {
+	} else if second != "" && third == "" {
 		c.NotFound = true
 	} else {
 		c.NotFound = true
 	}
 }
 func handleFreshIndex(c *router.Context) {
-	//c.SendContentInLayout("welcome.html", WelcomeIndexVars(c.Db, "created_at desc", ""), 200)
+	model := c.FindModel("story")
+	rows := c.SelectAllFrom(model, "order by created_at desc", c.EmptyParams())
+	c.SendContentInLayout("stories_index.html", rows, 200)
 }
--- app/model_story.go
+++ app/model_story.go
@@ -1,76 +1,11 @@
 package app
 
 import (
-	"fmt"
-	"html"
-	"html/template"
-	"strings"
-
-	"github.com/andrewarrow/feedback/models"
 	"github.com/andrewarrow/feedback/router"
-	"github.com/jmoiron/sqlx"
 )
-
-type Story struct {
-	Url       string
-	Title     string
-	Guid      string
-	Ago       string
-	Timestamp string
-	Username  string
-	HasUrl    bool
-	Domain    string
-	Body      template.HTML
-	Id        int64
-	Comments  int64
-	Points    int64
-}
 
 func FetchStory(c *router.Context, guid string) map[string]any {
 	model := c.FindModel("story")
 	params := []any{guid}
 	return c.SelectOneFrom(model, "where guid=$1", params)
-}
-
-func storyFromMap(m map[string]any) *Story {
-	story := Story{}
-	story.Title = models.RemoveMostNonAlphanumeric(fmt.Sprintf("%s", m["title"]))
-	story.Url = fmt.Sprintf("%s", m["url"])
-	story.Guid = fmt.Sprintf("%s", m["guid"])
-	story.Domain = fmt.Sprintf("%s", m["domain"])
-	story.Username = fmt.Sprintf("%s", m["username"])
-	story.Id = m["id"].(int64)
-	story.Comments = m["comments"].(int64)
-	story.Points = m["points"].(int64)
-	body := fmt.Sprintf("%s", m["body"])
-	body = strings.Replace(html.EscapeString(body), "\n", "<br/>", -1)
-	story.Body = template.HTML(body + "<br/><br/>")
-	if story.Url != "" {
-		story.HasUrl = true
-	}
-
-	//story.Timestamp, story.Ago = router.FixTime(m)
-	return &story
-}
-
-func FetchStories(db *sqlx.DB, order, domain string) []*Story {
-	stories := []*Story{}
-	params := []any{}
-	sql := fmt.Sprintf("SELECT * FROM stories ORDER BY %s limit 30", order)
-	if domain != "" {
-		sql = fmt.Sprintf("SELECT * FROM stories where domain=$1 ORDER BY %s limit 30", order)
-		params = append(params, domain)
-	}
-	rows, err := db.Queryx(sql, params...)
-	if err != nil {
-		return stories
-	}
-	defer rows.Close()
-	for rows.Next() {
-		m := make(map[string]any)
-		rows.MapScan(m)
-		story := storyFromMap(m)
-		stories = append(stories, story)
-	}
-	return stories
 }
