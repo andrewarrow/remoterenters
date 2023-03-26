54cb9aaefb89f9b08353543a5e8f5760c6115629
Andrew Arrow <oneone@gmail.com>
2023-03-26 14:31:21 -0700

    making web use same as api for create

--- app/stories_controller.go
+++ app/stories_controller.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/andrewarrow/feedback/models"
 	"github.com/andrewarrow/feedback/router"
-	"github.com/andrewarrow/feedback/util"
 )
 
 func HandleStories(c *router.Context, second, third string) {
@@ -23,50 +22,26 @@ func HandleStories(c *router.Context, second, third string) {
 
 func handleStoriesIndex(c *router.Context) {
 	if c.Method == "POST" {
+
 		title := strings.TrimSpace(c.Request.FormValue("title"))
 		url := strings.TrimSpace(c.Request.FormValue("url"))
 		body := strings.TrimSpace(c.Request.FormValue("body"))
-		returnPath := "/stories/new/"
-		if len(title) < 10 {
-			router.SetFlash(c, "title too short.")
-			http.Redirect(c.Writer, c.Request, returnPath, 302)
-			return
-		}
-		if len(title) > 140 {
-			router.SetFlash(c, "title too long.")
-			http.Redirect(c.Writer, c.Request, returnPath, 302)
-			return
-		}
-		if body == "" && url == "" {
-			router.SetFlash(c, "body or url required.")
-			http.Redirect(c.Writer, c.Request, returnPath, 302)
-			return
-		}
-		if url != "" {
-			if len(url) < 13 ||
-				!(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
-				router.SetFlash(c, "url too short.")
-				http.Redirect(c.Writer, c.Request, returnPath, 302)
-				return
-			}
-		}
-		if body != "" && len(body) < 10 {
-			router.SetFlash(c, "body too short.")
-			http.Redirect(c.Writer, c.Request, returnPath, 302)
-			return
-		}
-		guid := util.PseudoUuid()
 		sub := router.GetCookie(c, "sub")
-		if url != "" {
-			domain := util.ExtractDomain(url)
-			c.Db.Exec("insert into stories (points, sub, title, url, guid, username, domain) values ($1, $2, $3, $4, $5, $6, $7)", 1, sub, title, url, guid, c.User["username"], domain)
-		} else {
-			c.Db.Exec("insert into stories (points, sub, title, body, guid, username) values ($1, $2, $3, $4, $5, $6)", 1, sub, title, body, guid, c.User["username"])
+		c.Params = map[string]any{"title": title,
+			"url": url, "body": body, "sub": sub}
+
+		returnPath := "/stories/new"
+		message := c.ValidateJsonForModel("story")
+		if message != "" {
+			router.SetFlash(c, message)
+			http.Redirect(c.Writer, c.Request, returnPath, 302)
+			return
 		}
+
 		if sub == "" {
 			http.Redirect(c.Writer, c.Request, "/", 302)
 		} else {
-			http.Redirect(c.Writer, c.Request, "/rr/"+sub+"/", 302)
+			http.Redirect(c.Writer, c.Request, "/rr/"+sub, 302)
 		}
 		return
 	}
